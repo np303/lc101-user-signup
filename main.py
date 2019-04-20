ffb950a4abdbc2ad264f12775acfdb6894b5ed35
Nick Paisley <2ep303@gmail.com>
2019-04-20 18:34:52 -0500

    final user-signup

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
-from flask import Flask, request, redirect, render_template
-import cgi
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-
+import JS 
+import cgi
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+ 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+     
+@app.route('/signup', methods=['POST'])
+def signup():
+    #create variables for form inputs
+    username = request.form['username']
+    password = request.form['password']
+    password_verify = request.form['password-verify']
+    
+
+    #create a place for error messages. 
+
+    username_error = ""
+    password_error = ""
+    password_verify_error = ""
+    
+
+#Username Validation    
+    if username == "" and password == password_verify:
+        username_error = "Gotta have a user name, brah."
+        return render_template("index.html", username=username, username_error=username_error)
+    
+    elif password != password_verify:
+        password_error = "Passwords do NOT match!"
+        return render_template("index.html", password_error=password_error)
+        
+    else:    
+        return redirect('/welcome?username={0}'.format(username))
 
 
 
-@app.route("/signup", methods=['GET'])
-def user_signup():
-    return render_template('signup.html')
+@app.route("/welcome")
+def welcome():
+    username = request.args.get('username')
+    return render_template('welcome.html', username=username)
 
-
-
-
-
-
-app.run()
+app.run(debug=True,host='127.0.0.1', port=5000)
